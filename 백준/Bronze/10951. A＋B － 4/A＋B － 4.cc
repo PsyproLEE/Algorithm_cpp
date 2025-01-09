ddcb39ccccda36c604b2a2af8a007d6006eee815
PsyproLEE <166608484+PsyproLEE@users.noreply.github.com>
2025-01-09 21:23:54 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int a , b;
+    
+
+    while(1){
+        cin >> a >>b;
+        if(cin.eof()) break;
+        cout<<a+b<<'\n';
+
+    }
+}

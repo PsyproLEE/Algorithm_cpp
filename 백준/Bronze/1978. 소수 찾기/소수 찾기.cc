b40d6460cc72d7d2b9312e31c93392e67b74a635
PsyproLEE <166608484+PsyproLEE@users.noreply.github.com>
2025-01-11 14:57:11 +0900

    [Bronze II] Title: 소수 찾기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
@@ -1,32 +1,42 @@
-#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    vector<int> prime_num ;
-    for (int i =0 ; i<n; i++){
-        cin >> v[i];
+
+#include <iostream>
+using namespace std;
+
+vector<int> sieve(int n){
+    vector<int> primes;
+    vector<bool> state (n+1, true);
+    state[1] = false;
+    for (int i =2; i*i<=n; i++){
+        if(!state[i]) continue;
+        for (int j = i*i; j<=n; j+=i){
+            state[j] = false;
+        }
     }
+    for (int i =2; i<= n; i++){
+        if(state[i]) primes.push_back(i);
+    }
+    return primes;
+}
+
+int main() 
+{   
+    int n ; cin >> n;
+    vector <int> v(n);
+    vector <int> ans;
     int cnt = 0;
-    
-    for (int num : v){
-        if (num < 2) continue;
-        bool is_prime = true;
-        for(int i=2; i<= sqrt(num); i++){
-            if (num % i == 0){
-                is_prime = false;
-                break;
-            }
-        }
-        if (is_prime == true){
-            cnt ++;
-        }
-            
+    for (int i =0 ; i < n; i++){
+        int a; cin >> a;
+        v.push_back(a);
     }
+    sort(v.rbegin(), v.rend());
+    ans = sieve(v[0]);
+    for (int a: v){
+        for (int b: ans){
+            if (a == b) {cnt++; break;}
+            else if(a < b) {break;}
+        }
+    } 
     cout << cnt;
 }

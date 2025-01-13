3dbdebf229f1954815133b9daaf7684b5c419187
PsyproLEE <166608484+PsyproLEE@users.noreply.github.com>
2025-01-13 10:51:24 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -17,17 +17,18 @@ int main(void){
 
     int remove1 = -1, remove2 = -1;
     bool is_good = false;
-    for(int i =0; i<9 ; i++)
+    for(int i =0; i<9 ; i++){
         for(int j =0; j<9 ; j++){
             if(i == j) continue;
             if(sum - v[i] - v[j] == 100){ 
                 remove1 = i;
                 remove2 = j;
-                
                 is_good = true;
                 break;
                 }
-        if(is_good) break;}
+        }
+        if(is_good) break;    
+    }
     for(int i = 0; i<v.size() ; i++){
         if (i != remove1 && i != remove2)
             cout << v[i] << '\n';

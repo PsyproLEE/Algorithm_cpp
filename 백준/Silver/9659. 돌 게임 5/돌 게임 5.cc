f8695e15eb99b5c92a5dae8e0b0884c04e360381
PsyproLEE <166608484+PsyproLEE@users.noreply.github.com>
2025-02-17 10:25:50 +0900

    [Silver III] Title: 돌 게임 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long n; cin >> n;
+
+    if(n % 2 != 0) cout << "SK";
+    else cout << "CY";
+
+
+}

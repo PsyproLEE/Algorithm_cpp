423690f3bc7c1224c6fd6bdb5d73bc061d94c736
PsyproLEE <166608484+PsyproLEE@users.noreply.github.com>
2025-03-17 18:56:05 +0900

    [Silver III] Title: 단순한 문제 (Large), Time: 208 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/25487. 단순한 문제 （Large）/단순한 문제 （Large）.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+
+    while(t--){
+        int a, b, c;
+        cin >> a>> b>> c;
+        cout << min(a,min(b,c)) <<'\n';
+    }
+}

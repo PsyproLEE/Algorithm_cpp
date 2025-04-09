7ace3eaf348b437482ff7592f16e1ef127bb62cc
이지훈 <166608484+PsyproLEE@users.noreply.github.com>
2025-04-09 19:23:28 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 8 ms, Memory: 2812 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.cc
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.cc
@@ -1,28 +1,32 @@
 #include <iostream>
-#include <bits/stdc++.h>
-using namespace std;
-int main(void){
-    ios::sync_with_stdio(0); cin.tie(0);
-    int n, m; cin >> n >> m;
-    vector<vector<int>> v(n,vector<int>(m));
+#include <vector>
 
-    for (int i = 0 ;i< n; i++){
-        for (int j=0; j< m ; j++){
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> v(n+1,vector<int>(m+1,0));
+    vector<vector<int>> sum(n+1,vector<int>(m+1,0));
+
+
+    for(int i =1; i<=n; ++i){
+        for (int j =1; j<=m; ++j){
             cin >> v[i][j];
+            sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + v[i][j]; 
         }
     }
 
     int k ; cin >> k;
-    int i, j, x, y;
-    for (int l = 0; l < k ; l++){
-        long long sum = 0;
-        cin >> i >> j >> x >>y;
-        for (int o = i; o<= x; o++){
-            for (int p = j ; p <= y ; p++ ){
-                sum += v[o-1][p-1];
-            }
-        }
-        cout << sum << '\n';
+    while(k--){
+        int i, j ,x, y;
+        cin >> i >> j >> x >> y;
+        
+        cout << sum[x][y] - sum[i - 1][y] - sum[x][j - 1] + sum[i - 1][j - 1] << '\n';
+        
     }
 
+    
 }

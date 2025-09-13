566ef7bcf94d9d25f711d38ff19de51e591dcbec
이지훈 <166608484+PsyproLEE@users.noreply.github.com>
2025-09-13 13:02:18 +0900

    [Gold IV] Title: LCS 3, Time: 4 ms, Memory: 6048 KB -BaekjoonHub

---
+++ 백준/Gold/1958. LCS 3/LCS 3.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int dp[101][101][101] = {0,};
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    string A, B, C;
+    cin >> A >> B >> C;
+    
+    for (int i = 1; i <= A.size(); ++i) {
+        for (int j = 1; j <= B.size(); ++j) {
+            for (int k = 1; k <= C.size(); ++k) {
+                if (A[i-1] == B[j-1] && B[j-1] == C[k-1]) {
+                    dp[i][j][k] = dp[i-1][j-1][k-1] + 1;
+                } else {
+                    dp[i][j][k] = max({dp[i-1][j][k], dp[i][j-1][k], dp[i][j][k-1]});
+                }
+            }
+        }
+    }
+    
+    cout << dp[A.size()][B.size()][C.size()] << '\n';
+    
+    return 0;
+}

758da989867bccf720dc876319df3e580679d142
이지훈 <166608484+PsyproLEE@users.noreply.github.com>
2025-09-13 13:51:44 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
 using ll = long long;
 
@@ -9,32 +10,32 @@ int main()
     cin.tie(0);
     cout.tie(0);
     ios::sync_with_stdio(0);
-
-    int n; cin >> n;
-
-    vector<int> v(n);
-    for(int i =0; i<n; ++i){
-        cin >> v[i];
+    int n;
+    cin >> n;
+    int arr[1001], dp[1001] ={0,}, prev[1001];
+    fill(prev, prev + n, -1);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
     }
-
-    vector<int> dp(n,1) , prev(n, -1);
-
-    for(int i =0; i<n; ++i){
-        for(int j = 0; j< i; ++j){
-            if(v[j] < v[i] && dp[j] + 1 > dp[i]){
+    
+    for (int i =0; i<n;++i){   
+        dp[i] = 1;
+        for (int j =0; j<i; ++j){
+            if (arr[i]> arr[j] && dp[j]+1 > dp[i]){
                 dp[i] = dp[j] + 1;
                 prev[i] = j;
             }
-
         }
     }
 
-    int last = max_element(dp.begin(), dp.end()) - dp.begin();
+    int last = max_element(dp,dp+n) - dp;
     vector<int> lis;
-    for (int cur = last; cur != -1; cur = prev[cur]) lis.push_back(v[cur]);
+    for(int cur = last ; cur != -1; cur = prev[cur]){
+        lis.push_back(arr[cur]);
+    }
     reverse(lis.begin(), lis.end());
-    
-    cout << dp[last] << '\n';
-    for(int x : lis) cout << x << ' ';
+    cout << dp[last] <<'\n';
+    for(int x: lis) cout << x << ' ';
     return 0;
 }
